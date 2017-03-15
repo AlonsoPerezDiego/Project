f489584a368a3de5a47a1d5c4b8dcceeb1994488
AlonsoPerezDiego <Dialpe22@gmail.com>
2017-03-15 15:27:44 +0100

    Added File with all the information about Hakonmyo.

--- src/windows/Hakonmyo.java
+++ src/windows/Hakonmyo.java
@@ -5,6 +5,12 @@
  */
 package windows;
 
+import classpackage.Info;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+import static lbry.Lbry.fileNotFound;
+
 /**
  *
  * @author Diego
@@ -32,8 +38,10 @@ public class Hakonmyo extends javax.swing.JFrame {
         back = new javax.swing.JButton();
         menu = new javax.swing.JButton();
         exit = new javax.swing.JButton();
-        info = new javax.swing.JLabel();
-        name = new javax.swing.JLabel();
+        info1 = new javax.swing.JLabel();
+        info2 = new javax.swing.JLabel();
+        info3 = new javax.swing.JLabel();
+        nameH = new javax.swing.JLabel();
         background = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -74,13 +82,30 @@ public class Hakonmyo extends javax.swing.JFrame {
         });
         getContentPane().add(exit, new org.netbeans.lib.awtextra.AbsoluteConstraints(1180, 20, -1, -1));
 
-        info.setBackground(new java.awt.Color(255, 255, 255));
-        info.setOpaque(true);
-        getContentPane().add(info, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 450, 460, 100));
+        info1.setBackground(new java.awt.Color(255, 255, 255));
+        info1.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        info1.setOpaque(true);
+        getContentPane().add(info1, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 450, 460, 30));
 
-        name.setBackground(new java.awt.Color(255, 255, 255));
-        name.setOpaque(true);
-        getContentPane().add(name, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 200, 210, 250));
+        info2.setBackground(new java.awt.Color(255, 255, 255));
+        info2.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        info2.setOpaque(true);
+        getContentPane().add(info2, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 480, 460, 30));
+
+        info3.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        info3.setOpaque(true);
+        getContentPane().add(info3, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 510, 460, 30));
+
+        nameH.setBackground(new java.awt.Color(255, 255, 255));
+        nameH.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        nameH.setText("          PRESS HERE ");
+        nameH.setOpaque(true);
+        nameH.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                nameHMouseClicked(evt);
+            }
+        });
+        getContentPane().add(nameH, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 200, 210, 250));
 
         background.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Background.jpg"))); // NOI18N
         getContentPane().add(background, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
@@ -104,6 +129,33 @@ public class Hakonmyo extends javax.swing.JFrame {
         System.exit(0);
     }//GEN-LAST:event_exitActionPerformed
 
+    private void nameHMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_nameHMouseClicked
+        readFile();
+        
+    }//GEN-LAST:event_nameHMouseClicked
+
+    private Scanner scan;
+    private ArrayList <Info> list;
+    public void readFile(){
+        list = new ArrayList();
+        try{
+            scan = new Scanner(new File("HakonmyoInformation.txt"));
+            String[]aux;
+            while(scan.hasNextLine()){
+                aux = scan.nextLine().split(";");
+                Info object = new Info(aux[0],aux[1]);
+                list.add(object);
+            }
+        }catch(Exception ex){
+            fileNotFound();
+        }finally{
+            scan.close();
+        }
+        nameH.setText(list.get(0).getName());
+        info1.setText( list.get(0).getInformation());
+        info2.setText(list.get(1).getInformation());
+        info3.setText(list.get(2).getInformation());
+    }
     /**
      * @param args the command line arguments
      */
@@ -143,9 +195,11 @@ public class Hakonmyo extends javax.swing.JFrame {
     private javax.swing.JButton back;
     private javax.swing.JLabel background;
     private javax.swing.JButton exit;
-    private javax.swing.JLabel info;
+    private javax.swing.JLabel info1;
+    private javax.swing.JLabel info2;
+    private javax.swing.JLabel info3;
     private javax.swing.JButton menu;
-    private javax.swing.JLabel name;
+    private javax.swing.JLabel nameH;
     private javax.swing.JLabel pictureFront;
     // End of variables declaration//GEN-END:variables
 }
